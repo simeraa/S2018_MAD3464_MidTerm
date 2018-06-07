9711b26dea9fbc382a880b2658b9d6b03dcead0e
MacStudent <macstudent@20X-iMacXX-M-436.local>
2018-06-07 16:50:05 -0400

    fourth

--- 736437_MIDTERM_MADS3464/src/pkg736437_midterm_mads3464/LambtonStringTools.java
+++ 736437_MIDTERM_MADS3464/src/pkg736437_midterm_mads3464/LambtonStringTools.java
@@ -21,7 +21,47 @@ public class LambtonStringTools {
           
       }
       return reverseStringVal;
-  }}
+  }
+ public static int getDecimal(int binary){  
+    int decimal = 0;  
+    int n = 0;  
+    while(true){  
+      if(binary == 0){  
+        break;  
+      } else {  
+          int temp = binary%10;  
+          decimal += temp*Math.pow(2, n);  
+          binary = binary/10;  
+          n++;  
+       }  
+    }  
+  return decimal;
+ }
+    
+  class MaxOccurenceOfCharacter {
+
+    public static String mostFrequent(String input) {
+        char[] array = input.toCharArray();
+        int maxCount = 1;
+        char maxChar = array[0];
+ 
+          for(int i = 0, j = 0; i < input.length() - 1; i = j){
+               int count = 1;
+           while (++j < input.length() && array[i] == array[j]) {
+                  count++;
+                }
+         
+          if (count >= maxCount) {
+             maxCount = count;
+             maxChar = array[i];
+         }  }
+ 
+    return (maxChar + " = " + maxCount);
+    }
+}   
+}
+          
+
   
  
 
--- 736437_MIDTERM_MADS3464/src/pkg736437_midterm_mads3464/Main.java
+++ 736437_MIDTERM_MADS3464/src/pkg736437_midterm_mads3464/Main.java
@@ -1,5 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+ * To change this license header, choose License Headers in Project Propertie
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -17,5 +17,24 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(LambtonStringTools.reverseString("Lambton"));
     }
-    
+    public static void main(String args[]){    
+System.out.println("Decimal of 1010 is: "+getDecimal(1010));  
+System.out.println("Decimal of 10101 is: "+getDecimal(10101));  
+System.out.println("Decimal of 11111 is: "+getDecimal(11111));  
+  }
+
+    private static String getDecimal(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+             class SubString {
+
+     public static String replaceSubString(String s1, String s2, String s3) {
+        
+        s1 = s1.replaceAll(s2,s3);
+        return s1;
+    }
 }
+}    
+        
+    
+
